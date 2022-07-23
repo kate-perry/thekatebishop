e37f2959ab17228152ceaba5dece20a84778aad9
Kate Perry <thekateperry@gmail.com>
2022-07-23 00:40:34 -0400

    some animations, additional header content

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <AboutMe />
+      {/* <AboutMe /> */}
     </div>
   );
 }
--- src/features/Header.tsx
+++ src/features/Header.tsx
@@ -8,7 +8,8 @@ import {
     tagline1,
     tagline2,
     aboutMe
-} from '../shared/utils/strings'
+} from '../shared/utils/strings';
+import katebishop from '../shared/static/katebishop.png';
 
 export default function Header() {
     const [showContent, setShowContent] = useState(false);
@@ -21,38 +22,50 @@ export default function Header() {
             <div className="background" />
             <div className="content">
                 <Grid container direction="row">
-                    <CSSTransition
-                        in={showContent}
-                        timeout={transitionTimeout}
-                        classNames="content-left"
-                        unmountOnExit
-                        onEnter={() => setShowContent(true)}>
-                        <Grid container direction="column">
-                            <Grid item>
-                                <h6>{tagline1}</h6>
+                    <Grid item xs={6}>
+                        <CSSTransition
+                            in={showContent}
+                            timeout={transitionTimeout}
+                            classNames="content-left"
+                            unmountOnExit
+                            onEnter={() => setShowContent(true)}>
+                            <Grid container direction="column">
+                                <Grid item>
+                                    <h6>{tagline1}</h6>
+                                </Grid>
+                                <Grid item>
+                                    <h1>{title.toUpperCase()}</h1>
+                                </Grid>
                             </Grid>
-                            <Grid item>
-                                <h1>{title.toUpperCase()}</h1>
+                        </CSSTransition>
+                        <CSSTransition
+                            in={showContent}
+                            timeout={transitionTimeout}
+                            classNames="content-bottom"
+                            unmountOnExit
+                            onEnter={() => setShowContent(true)}>
+                            <Grid container direction="column">
+                                <Grid item>
+                                    <h5>
+                                        <span>{tagline2}</span>
+                                    </h5>
+                                </Grid>
+                                <Grid item>
+                                    <p>{aboutMe}</p>
+                                </Grid>
                             </Grid>
-                        </Grid>
-                    </CSSTransition>
-                    <CSSTransition
-                        in={showContent}
-                        timeout={transitionTimeout}
-                        classNames="content-bottom"
-                        unmountOnExit
-                        onEnter={() => setShowContent(true)}>
-                        <Grid container direction="column">
-                            <Grid item>
-                                <h5>
-                                    <span>{tagline2}</span>
-                                </h5>
-                            </Grid>
-                            <Grid item>
-                                <p>{aboutMe}</p>
-                            </Grid>
-                        </Grid>
-                    </CSSTransition>
+                        </CSSTransition>
+                    </Grid>
+                    {/* <Grid item xs={6} alignContent="flex-end">
+                        <CSSTransition
+                            in={showContent}
+                            timeout={transitionTimeout}
+                            classNames="content-grow"
+                            unmountOnExit
+                            onEnter={() => setShowContent(true)}>
+                            <img src={katebishop} alt="Photo of Kate Bishop" />
+                        </CSSTransition>
+                    </Grid> */}
                 </Grid>
             </div>
         </div>
