c53b3bd58a9d58ef7127c79bc4c58b0423622a58
Kate Perry <thekateperry@gmail.com>
2022-07-23 21:16:37 -0400

    window-size responsive formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import Header from './features/Header';
 import AboutMe from './features/AboutMe';
 import './shared/styles/styles.scss';
+import { getWindowSize } from './shared/utils/functions';
+import { WindowSize } from './shared/utils/constants';
+
+export const WindowSizeContext = createContext(WindowSize.MEDIUM);
 
 function App() {
+  const [windowSize, setWindowSize] = useState(WindowSize.MEDIUM);
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize())
+    }
+    window.addEventListener('resize', handleWindowResize);
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+
   return (
     <div className="app">
-      <Header />
-      {/* <AboutMe /> */}
+      <WindowSizeContext.Provider value={windowSize} >
+        <Header />
+        {/* <AboutMe /> */}
+      </WindowSizeContext.Provider>
     </div>
   );
 }
